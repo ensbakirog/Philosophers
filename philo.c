53aa0c578dc76dc09ddd9bf39854cc8b15496114
Enes Bakıroğlu <ensbakirog@gmail.com>
2024-01-27 00:01:12 +0300

    Arguman hataları düzgün bir şekilde çalışıyor. argumanları kontrol eden if kaldırıldı yerine başka bir fonksiyona yolladım orda kontrol ediyor.

--- philo.c
+++ philo.c
@@ -1,13 +1,28 @@
 #include <unistd.h>
 #include "philo.h"
 
+int check_all(char **av, int count)
+{
+	if (ft_atoi(av[0]) < 1)
+		return (-1);
+	else if (ft_atoi(av[1]) < 0)
+		return (-1);
+	else if (ft_atoi(av[2]) < 0)
+		return (-1);
+	else if (ft_atoi(av[3]) < 0)
+		return (-1);
+	else if (count == 5 && ft_atoi(av[4]) < 0)
+		return (-1);
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
 	if (ac == 5 || ac == 6)
 	{
-		if ((ft_atoi(av[1]) < 1 || ft_atoi(av[2]) < 0 || ft_atoi(av[3]) < 0
-			|| ft_atoi(av[4]) < 0) || (ac == 6 && ft_atoi(av[5]) < 0))
-				return (write(2, "Error: Wrong argument\n", 22), 1);
+		av++;
+		if (check_all(av, ac - 1) == -1)
+			return (write(2, "Error: Wrong argument\n", 22), 1);
 		return (0);
 	}
 	return (write(2, "Error: Invalid argument\n", 24), 1);
--- philo.h
+++ philo.h
@@ -1,6 +1,14 @@
 #ifndef PHILO_H
 # define PHILO_H
 
+#include <pthread.h>
+
+typedef struct s_philo
+{
+	int	philo_id;
+	pthread_t *thread;
+}	t_philo;
+
 int	ft_atoi(char *str);
 
 #endif
