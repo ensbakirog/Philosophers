51d7b69cd3c08222c1b393f7f3eda41f67ed600f
Enes Bakıroğlu <ensbakirog@gmail.com>
2024-01-26 18:07:04 +0300

    Arguman sayısı 5 veya 6 değilse hata bastıran kısım eklendi.

--- philo.c
+++ philo.c
@@ -1,4 +1,10 @@
-int	main(int ac, char const **av)
+#include <unistd.h>
+
+int	main(int ac, char **av)
 {
-	return	(0);
+	if (ac == 5 || ac == 6)
+	{
+
+	}
+	return (write(2, "Error: Invalid argument\n", 24), 1);
 }
